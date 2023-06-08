d5a34faaafe3fbc412dbb9d201cc6b4b6adbc749
Abelti <abeltibbeshana@gmail.com>
2023-06-08 08:46:52 +0300

    home-component-initial

--- chat/src/components/Home.jsx
+++ chat/src/components/Home.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 export  function HomeComponent () {
     return (
-        <div className='container mx-auto my-auto bg-black'>
-        <h1 className='title'>HOME PAGE</h1>
-        <button className='btn-register'>Go To Register Page</button>
-        <button className='btn-login'>Go To Login Page</button>
+        <>
+        <div className='bg-gray-900 h-screen'>
+            <div className='p-20'>
+                <h1 className='p text-4xl text-white font-bold text-center'>WELCOME! 👋</h1>
+            </div>
+            <div className='flex'>
+                <button className='w-1/3 my-5 ml-40 mr-20 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Go To Register Page</button>
+                <button className='w-1/3 my-5 ml-20 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Go To Login Page</button>
+            </div>
         </div>
+        </>
     );
 };
